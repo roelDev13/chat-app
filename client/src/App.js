a1874949c8cc20979596182b2ab3af3a032811a5
Roel Montoya <phoenixwings48@gmail.com>
2022-11-09 14:54:51 -0600

    roeldev: fixed client folder

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import Chat from "./Chat";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  const userNameHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const roomHandler = (event) => {
+    setRoom(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      { !showChat ? (
+      <div className="joinChatContainer">
+        <h3>Join A Chat</h3>
+        <input type="text" placeholder="Username..." onChange={userNameHandler} />
+        <input type="text" placeholder="Room ID..." onChange={roomHandler} />
+        <button onClick={joinRoom}>Join A Room</button>
+      </div>
+      ) : (
+      <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+}
+
+export default App;
